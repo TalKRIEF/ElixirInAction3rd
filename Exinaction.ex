1e503bc88a84844b56ee939894ee3b1b05d27bd9
Tal KRIEF <talkrief@gmail.com>
2025-11-04 18:13:13 +0100

    case max/2

--- Exinaction.ex
+++ Exinaction.ex
@@ -1,1 +1,9 @@
-# 1st commit
+# Kernel.max/2 existe, donc spécifier Numbers.max/2 pour appeler ma propre fonction
+defmodule Numbers do
+  def max(a, b) do
+    case a >= b do
+      true -> "vrai"
+      false -> "faux"
+    end
+  end
+end
